67f4afec76726b869d6fe08e6f1e37c61249adf6
DilsharaNiromali <127034684+DilsharaNiromali@users.noreply.github.com>
2024-05-06 22:20:54 +0530

    test cases - bidding round

--- test/test_auction.js
+++ test/test_auction.js
@@ -1,7 +1,7 @@
 //importing TestAuction contract and other relevant libraries.
 let Auction = artifacts.require("./TestAuction.sol");
 const truffleAssert = require("truffle-assertions");
-const { Time } = require("@openzeppelin/test-helpers");
+const { time } = require("@openzeppelin/test-helpers");
 
 //defining the tests within a contract block which provides access to etherium accounts.
 contract("Auction", accounts => {
@@ -18,6 +18,7 @@ contract("Auction", accounts => {
     const ONE_DAY = 86400;
     const BANDWIDTH = 200;
     const MIN_BID_VALUE = 50000;
+    const MIN_USAGE_TIME = 1800;
     const DEPOSIT_VALUE = 100000;
     //defining an array for test bid values.
     const TEST_BIDS = [MIN_BID_VALUE , MIN_BID_VALUE + 1, MIN_BID_VALUE + 3, MIN_BID_VALUE + 4, MIN_BID_VALUE+2];
@@ -35,7 +36,122 @@ contract("Auction", accounts => {
         );
     });
 
+    it("contract is initialized with correct parameters and deadlines", async () => {
 
+        let Auction_Info  = await getAuctionInfo();
+        // get the latest time using the Time helper function
+        const latestTime = await time.latest();
+        //verifying that the current parameter values of the auction matches the expected parameter values
+        expect(Auction_Info.currentState).to.equal(STATE_READY_FOR_BIDS);
+        expect(Auction_Info.PU).to.equal(PU_ACCOUNT);
+        expect(Auction_Info.bandwidth).to.equal(BANDWIDTH);
+        expect(Auction_Info.minBidValue).to.equal(MIN_BID_VALUE);
+        expect(Auction_Info.depositValue).to.equal(DEPOSIT_VALUE);
+        expect(Auction_Info.BidsDeadline).to.equal(latestTime.toNumber() + ONE_DAY);
+        expect(Auction_Info.BidsRevealDeadline).to.equal(latestTime.toNumber() + (ONE_DAY * 2));
+    });
+
+    //defining a function to retrieve auction information from the test contract.
+    //return the current state of the auction and its parameters
+    getAuctionInfo= async () => {
+        let info = await testContract.getAuctionInfo.call();
+        return {
+            "currentState": Number(info[0]),
+            "PU": info[1],
+            "bandwidth": Number(info[2]),
+            "minBidValue": Number(info[3]),
+            "depositValue": Number(info[4]),
+            "BidsDeadline": Number(info[5]),
+            "BidsRevealDeadline": Number(info[6]),
+        };
+    }
+
+     //test case to check whether SUs can bid successfully in the first round
+    it("can place bids successfully", async () => {
+        //setting the current state to ready for bids state.
+        await testContract.setCurrentState(STATE_READY_FOR_BIDS);
+        //placing a bid in the bidding round
+        await bidInBiddingRound(MIN_BID_VALUE,MIN_USAGE_TIME, accounts[1], DEPOSIT_VALUE);
+        //retrieving the bid information
+        const bid = await testContract.bids.call(accounts[1]);
+        //checking whether the bid exits or not
+        expect(bid.existsBid).to.equal(true);
+       
+    });
+
+    it("stores bid information correctly when a bid is placed", async () => {
+        //placing a bid in the bidding round
+        await bidInBiddingRound(MIN_BID_VALUE, MIN_USAGE_TIME,accounts[1], DEPOSIT_VALUE);
+        //retrieving the bid information
+        const bid = await testContract.bids.call(accounts[1]);
+        //verifying that the bid information is stored correctly.
+        expect(bid.existsBid).to.equal(true);
+        expect(bid.Bidding).to.equal(web3.utils.soliditySha3(MIN_BID_VALUE,MIN_USAGE_TIME,"some_salt"));
+        expect(Number(bid.BidReveal)).to.equal(0);
+        expect(Number(bid.minUsageTime)).to.equal(0);
+        expect(bid.isBidRevealValid).to.equal(false);
+        expect(Number(bid.deposit)).to.equal(DEPOSIT_VALUE);
+             
+    });
+
+    it("adds SU's address to BidsAddresses array when a bid is placed", async () => {
+        //placing a bid 
+        await bidInBiddingRound(MIN_BID_VALUE,MIN_USAGE_TIME, accounts[1], DEPOSIT_VALUE);
+        //retrieving the length of the bidsAdrreses array from the contract
+        const bidsLength = await testContract.getBidsLength();
+        //checking whether the bidders address is added to the array
+        expect(Number(bidsLength)).to.equal(1);  
+   });
+
+   it("cannot place a bid when in an incorrect state",async() =>{
+    //set the current state of the bid to STATE_CLOSED
+        await testContract.setCurrentState(STATE_CLOSED);
+    //checking whether the function reverts when palcing a bid with an incorrect state.
+        await truffleAssert.reverts(
+        bidInBiddingRound(MIN_BID_VALUE,MIN_USAGE_TIME, accounts[1], DEPOSIT_VALUE),
+        "Invalid state "
+    );
+
+   });
+
+   it("cannot place a bid in the bidding round after the deadline",async() =>{
+    //increasing the time to be more than the bidding round deadline.
+        await time.increase(ONE_DAY + 1);
+    //checking whether the function reverts when palcing a bid after the deadline.
+        await truffleAssert.reverts(
+        bidInBiddingRound(MIN_BID_VALUE, MIN_USAGE_TIME,accounts[1], DEPOSIT_VALUE),
+        "Cannot bid after deadline "
+    );
+
+   });
+
+
+   it("cannot place a bid if the deposit amount is lower than the expected amount",async() =>{
+   //checking whether the function reverts when palcing a bid with a low deposit amount that the minimum deposit value.
+    await truffleAssert.reverts(
+        bidInBiddingRound(MIN_BID_VALUE,MIN_USAGE_TIME,accounts[1],DEPOSIT_VALUE-1),
+        "Deposit value is too low"
+    ); 
+   });
+
+   //it("",async() =>{ });
+
+
+   //defining a function to place a bid in the first round.
+   bidInBiddingRound = async (bidValue, bidTime, bidderAddress, depositValue) => {
+    //calling the bidInBiddingRound while passing hashed bit value as the argument.
+    let tx = await testContract.bidInBiddingRound(web3.utils.soliditySha3(bidValue,bidTime,"some_salt"),
+     {
+        value: depositValue,
+        from: bidderAddress
+    });
+    //verifying that the ReceivedBid event is emiited correctly.
+    truffleAssert.eventEmitted(tx, "ReceivedBid", (ev) => {
+     return ev.SU == bidderAddress && ev.deposit == depositValue;
+    });  
+};
+
+   
     //tests
 
 });
